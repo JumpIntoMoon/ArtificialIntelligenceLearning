3f85e22bfc09a2da04329fc25f47f66552fa3dc6
tangyilong <t_yl@outlook.com>
2018-04-13 15:50:05 +0800

    聊天机器人代码--初版

---
+++ venv/Include/chatBot/chatBot.py
@@ -0,0 +1,25 @@
+import random
+#打招呼
+greetings = ['hola', 'hello', 'hi','Hi','hey!','hey','你好','你好！']
+
+nameQuestions = ['你是谁？', '你叫什么名字？']
+nameResponses = ['我是Aka，你好呀！',"我的名字叫Aka~~"]
+
+ageQuestions = ['你几岁？', '你多大啦？']
+ageResponses = ['我11岁哦！',"我11岁啦~~"]
+
+byebye =['bye','byebye','再见']
+
+while True:
+	userInput = input(">>> ")
+	if userInput.lower() in greetings:
+		print(random.choice(greetings))
+	elif userInput in nameQuestions:
+		print(random.choice(nameResponses))
+	elif userInput in ageQuestions:
+		print(random.choice(ageResponses))
+	elif userInput.lower() in byebye:
+		print(random.choice(byebye))
+		break
+	else:
+		print("I did not understand what you said, please try something else.")
